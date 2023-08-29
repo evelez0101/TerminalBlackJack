896a5020e71bc0ff362f9f1fc40d633185a64b2f
Evelio Velez <114781973+evelez0101@users.noreply.github.com>
2023-08-29 00:27:49 -0400

    Update runner.java

--- runner.java
+++ runner.java
@@ -1,16 +1,7 @@
 public class runner {
     public static void main(String [] args)
     {
-        Deck d = new Deck();
-        Player p = new Player();
-        
-        // Testing dealing to hands
-        p.addCardtoHand(d.drawCard());
-        p.addCardtoHand(d.drawCard());
-
-        // Shows Hand 
-        p.displayHand();
-
-        System.out.println(p.handValue().toString());
+        // Starts the Game
+        new GameLogic ();
     }
 }
