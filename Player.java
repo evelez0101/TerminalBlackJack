f1673a6f79f789b783618831136bbc5af5a188c3
Evelio Velez <114781973+evelez0101@users.noreply.github.com>
2023-08-29 00:26:55 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class Player 
 {
     // Card Hand
-    private int buyIn;
     private ArrayList<Card> hand;
     
     public Player()
@@ -11,20 +10,79 @@ public class Player
         hand = new ArrayList<Card>();
     }
 
+    // Adds Card to hand
     public void addCardtoHand(Card c)
     {
         hand.add(c);
     }
 
-    public void displayHand()
+    // Calculates two potential Sums in case of Ace
+    public void displayScore()
     {
-        for( Card c: hand)
+        int [] scores = handValue();
+        
+        // Ony two values will ever be returned 
+        if (scores[0] == scores[1])
         {
-            System.out.print(c);
+            // If the values are equal
+            System.out.println("\t\tCurrent Value: " + scores[0]);
+        }
+        else
+        {
+            // If the values are different
+            System.out.println("\t\tCurrent Values: " + scores[0] + " or " + scores[1]);
         }
     }
 
-    public int [] handValue()
+    public boolean hasBust()
+    {
+        // Check values
+        int [] scores = handValue();
+
+        // Need come back and fix this to consider edge cases
+        if (scores[0] > 21 || scores[1] > 21 )
+        {
+            return true;
+        }
+
+        return false;   
+    }
+
+
+    public void displayHand()
+    {
+        // Array of Arrays
+        String [] [] cards = new String[hand.size()] [];
+
+        // Populate Array
+        for(int i = 0; i < hand.size(); i++)
+        {
+            cards[i] = hand.get(i).cardCreate();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        // Displays the Cards line by line
+        for(int cardRow = 0; cardRow < hand.get(0).cardCreate().length; cardRow++)
+        {
+            for (int card = 0; card < hand.size(); card++)
+            {
+                sb.append(" " + cards[card][cardRow]);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public boolean hasBlackjack()
+    {
+        // Checks that the first two cards are an ace and a "ten" card
+        return (hand.get(0).getValue() >= 10 || hand.get(1).getValue() >= 10) 
+                && (hand.get(0).getValue() == 1 || hand.get(1).getValue() == 1);
+    }
+
+    private int [] handValue()
     {
         // Calculates two sums in case of an ace
         int val1 = 0; 
@@ -34,15 +92,29 @@ public class Player
         for (Card c: hand)
         {
             // Handles Ace 
-            if (c.getValue() == 1)
+            switch(c.getValue())
             {
-                val2 += 11;
-                val1 += 1;
-                continue;
-            } 
-
-            val1 += c.getValue();
-            val2 += c.getValue();
+                // Ace
+                case 1:  val2 += 11; 
+                         val1 += 1;
+                         break;
+                // Jack
+                case 11: val2 += 10; 
+                         val1 += 10;
+                         break;
+                // Queen
+                case 12: val2 += 10; 
+                         val1 += 10;
+                         break;
+                // King
+                case 13: val2 += 10; 
+                         val1 += 10;
+                         break;
+                // All other Cards
+                default: val1 += c.getValue();
+                         val2 += c.getValue();
+                         break;
+            }
         }
 
         // Return both possible hand values if there is an ace
