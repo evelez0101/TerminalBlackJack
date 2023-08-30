ade9a98e55d381aad14f7a703465c36926187c6e
Evelio Velez <114781973+evelez0101@users.noreply.github.com>
2023-08-30 11:04:16 -0400

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -19,15 +19,23 @@ public class GameLogic
 
     public void displayGame()
     {  
-        System.out.println("\n\t\t== Dealer Hand == ");
+        displayLine();
+        System.out.println("\n\t\t\t== Dealer Hand == ");
         house.displayHand();
         house.displayScore();
 
+        // One of the dealers cards has to be hidden until it flipped 
+        //TODO
 
-        System.out.println("\n\t\t== Your Hand ==");
+        System.out.println("\n\t\t\t== Your Hand ==");
         player.displayHand();
         player.displayScore();
+        displayLine();
+    }
 
+    private void displayLine()
+    {
+        System.out.println("-----------------------------------------------------------");
     }
 
 
@@ -59,67 +67,135 @@ public class GameLogic
         }
         else
         {
-            System.out.println("\t\t== Black Jack ==");
+            System.out.println("\t\t\t== Black Jack ==");
+            return;
         }
         
         // Dealer Moves
-        dealerMove();
+        if(!player.hasBust())
+        {
+            dealerMove();
+        }
+
+        displayGame();
+
+        calculateWinner();
     }
 
     private void playerMove()
     {
         //Player Hit
-        int reply = -1;
+        int reply;
        
-        do
-        {
-            System.out.println("\nSelect One of the following Options");
-            System.out.println("\t 1.) Hit");
-            System.out.println("\t 2.) Stand");
+        
+        // Resets the reply with each iteration 
+        reply = -1;
 
-            try
-            {
-                reply = scan.nextInt();
-            } 
-            catch (Exception e)
-            {
-                System.out.println("Please enter a numeric value");
-            }
+        System.out.println("\n\t\tSelect One of the following Options");
+        System.out.println("\t\t 1.) Hit");
+        System.out.println("\t\t 2.) Stand");
             
+        // Error handling 
+        try
+        {
+            reply = scan.nextInt();
+        } 
+        catch (Exception e)
+        {
+            System.out.println("Please enter a numeric value");
+        }
+            
+        // Consider Case // If you stand or hit on an ace (19 or 29) 
 
-            if (reply == 1)
-            {
-                player.addCardtoHand(GameDeck.drawCard());
-                displayGame();
+        // Player Hit
+        if (reply == 1)
+        {
+            player.addCardtoHand(GameDeck.drawCard());
+            displayGame();
 
-                if (player.hasBust())
-                {
-                    System.out.println("You have gone Bust!");
-                    return;
-                }
-            }
-            else if(reply == 2)
+            if (player.hasBust())
             {
+                System.out.println("\t\t\tYou have gone Bust!");
                 return;
             }
-            else
-            {
-                System.out.println("Please enter a valid response");
-            }
 
-        } while (true);
-
-        //Player Stand
+            playerMove();
+        }
+         // Player Stand 
+        else if (reply == 2)
+        {
+        return;
+        }
+        else
+        {
+            System.out.println("Please enter a valid response");
+            playerMove();
+        }
     }
 
     private void dealerMove()
     {
         // Dealer Hit
+        int [] value = house.handValue();
 
-        // Dealer Stand (Hard 17 Rules)
-
+        // If there are no aces 
+        if (value[0] == value[1])
+        {
+            // Dealer has to hit on anything less than 17
+            if (value[0] < 16)
+            {
+                // Dealer Move
+                house.addCardtoHand(GameDeck.drawCard());
+                
+                // Has to decide again whether to hit or stand
+                dealerMove();
+            }
+            else
+            {
+                // Stand 
+                return;
+            }
+        }
+        else 
+        {
+            // If either hand is a above 16 dealer must stand
+            if (value[0] > 16 && value[1] > 16)
+            {
+                // Stand
+                return;
+            }
+            else 
+            {   
+                // Dealer Move
+                house.addCardtoHand(GameDeck.drawCard());
+                
+                // Has to decide again whether to hit or stand
+                dealerMove();
+            }
+        }
     }
 
+    public void calculateWinner()
+    {
+        int [] houseHand = house.handValue();
+        int [] playerHand =  player.handValue();
 
+        // Assigns the highest of the two values that is 
+        int houseBest = ( (houseHand[1] > houseHand[0]) && (houseHand[1] <= 21) ) ? houseHand[1] : houseHand[0];
+        int playerBest = ( (playerHand[1] > playerHand[0]) && (playerHand[1] <= 21) ) ? playerHand[1] : playerHand[0];
+
+    
+        if ( (playerBest <= 21) && (playerBest > houseBest))
+        {
+            System.out.println("\t\t\tPlayer Wins");
+        }
+        else if ( (houseBest <= 21) && (playerBest < houseBest))
+        {
+            System.out.println("\t\t\tHouse Wins");
+        }
+        else
+        {
+            System.out.println("\t\t\tDraw!");
+        }
+    }
 }
-
